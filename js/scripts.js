b79f3af63d4668b8d5e311e0909f20d966f611a2
moni-tu <turani.monica@gmail.com>
2021-10-04 22:55:13 +0200

    following instruction exercise 1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+alert('Hello World');
 
-let size= 100;
-let doubleSize= size*2;
-let minSize= (doubleSize*2)-(size/2);
-document.write(minSize);
+let favouriteFood = 'Lasagna';
+document.write(favouriteFood);
+favouriteFood = 'Pasta';
+document.write(favouriteFood);
+
+
 
  
