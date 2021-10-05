ff643dcb26007b646e40ae0c42060c860f8e997e
moni-tu <turani.monica@gmail.com>
2021-10-05 23:54:04 +0200

    follow instructions Achievement 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,20 @@ let anne = {
 console.log(anne.name);
 
 
+let currentUserName = 'Sam';
+
+let userAges = {
+    anne=27,
+    sam=112,
+    megan=97
+};
+
+userAges[currentUserName]=113;
+console.log(userAges[currentUserName]);
+
+let myVariable = 'Hello World';
+console.log(typeof myVariable);
+
 
 
  
