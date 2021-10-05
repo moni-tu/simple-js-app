c8ad10385c3f8c5163650d1ce7d4c804eecd8fea
moni-tu <turani.monica@gmail.com>
2021-10-05 10:04:47 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let favouriteFood = 'Lasagna';
 document.write(favouriteFood);
 
 let name='John';
-document.write('my name is'+name+'and I am ready to start');
+document.write('my name is '+name+' and I am ready to start');
 
 
 
