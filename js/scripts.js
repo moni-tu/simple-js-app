652555bc1a34f4c51f7e097295b55df0e9b921ed
moni-tu <turani.monica@gmail.com>
2021-10-08 18:48:19 +0200

    refactor: change variable and style: add line break
    refactor: change to pokemon with shorter height (diglett) so it will be considered by the condition <0.2.
    style: add line break between loop elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
     {name:'bulbasaur', height:0.7, type: ['grass','poison']},
     {name:'charmender', height:0.6, type: ['fire']},
-    {name:'squirtle', height:0.5, type: ['water']},
+    {name:'diglett', height:0.2, type: ['earth']},
     {name:'jigglipuff', height:0.5, type: ['fairy', 'normal']},
     {name:'gastly', height:1.3, type: ['ghost','poison']}
 ]
@@ -9,10 +9,13 @@ let pokemonList = [
 for (let i=0; i<pokemonList.length; i++) {
     if (pokemonList[i].height>1.0){
         document.write(pokemonList[i].name + 'height ' + pokemonList[i].height + 'Your Pokemon is big!');
+        document.write('<br>')
     } else if (pokemonList[i].height<1.0 && pokemonList[i].height>0.2){
         document.write(pokemonList[i].name + 'height ' + pokemonList[i].height);
+        document.write('<br>')
     } else {
         document.write(pokemonList[i].name + 'height ' + pokemonList[i].height + 'Your Pokemon is small!');
+        document.write('<br>')
     }
      
 }
