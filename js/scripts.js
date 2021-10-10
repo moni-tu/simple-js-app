314b517ecf16cc53b4e858741bb8ea53f6dfcf3b
moni-tu <turani.monica@gmail.com>
2021-10-10 23:25:29 +0200

    style: change Pokemons name
    Add capital letter to Pokemons names for better readability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 let pokemonList = [
-    {name:'bulbasaur', height:0.7, type: ['grass','poison']},
-    {name:'charmender', height:0.6, type: ['fire']},
-    {name:'diglett', height:0.2, type: ['earth']},
-    {name:'jigglipuff', height:0.5, type: ['fairy', 'normal']},
-    {name:'gastly', height:1.3, type: ['ghost','poison']}
+    {name:'Bulbasaur', height:0.7, type: ['grass','poison']},
+    {name:'Charmender', height:0.6, type: ['fire']},
+    {name:'Diglett', height:0.2, type: ['earth']},
+    {name:'Jigglipuff', height:0.5, type: ['fairy', 'normal']},
+    {name:'Gastly', height:1.3, type: ['ghost','poison']}
 ]
 
 for (let i=0; i<pokemonList.length; i++) {
