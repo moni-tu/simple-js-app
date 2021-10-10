998eb0d0cac482e686dc9d5e9a245447658c51a8
moni-tu <turani.monica@gmail.com>
2021-10-10 23:21:42 +0200

    style: add spacing and brackets
    Separating Pokemons height from their name trough spacings. Include height in brackets as requested by assignment 1.3.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,13 @@ let pokemonList = [
 
 for (let i=0; i<pokemonList.length; i++) {
     if (pokemonList[i].height>1.0){
-        document.write(pokemonList[i].name + 'height ' + pokemonList[i].height + 'Your Pokemon is big!');
+        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ') Your Pokemon is big!');
         document.write('<br>')
     } else if (pokemonList[i].height<1.0 && pokemonList[i].height>0.2){
-        document.write(pokemonList[i].name + 'height ' + pokemonList[i].height);
+        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ')');
         document.write('<br>')
     } else {
-        document.write(pokemonList[i].name + 'height ' + pokemonList[i].height + 'Your Pokemon is small!');
+        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ') Your Pokemon is small!');
         document.write('<br>')
     }
      
