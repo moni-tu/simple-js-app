9718f9864cb2e7ec8e7b29712bf6a2ef32a195e0
moni-tu <turani.monica@gmail.com>
2021-10-15 22:00:57 +0200

    refactor: re-name forEach loop as pokemonRepository.getAll().forEach

--- js/scripts.js
+++ js/scripts.js
@@ -18,24 +18,10 @@ let pokemonRepository = (function() {
 
 })();
 
-document.write(pokemonRepository.getAll());
 pokemonRepository.add({name: 'Psyduck', height: 0.8});
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     console.log(pokemon.name + ', height: ' + pokemon.height + ', type: ' + pokemon.type);
 });
 
-for (let i=0; i<pokemonList.length; i++) {
-    if (pokemonList[i].height>1.0){
-        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ') Your Pokemon is big!');
-        document.write('<br>')
-    } else if (pokemonList[i].height<1.0 && pokemonList[i].height>0.2){
-        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ')');
-        document.write('<br>')
-    } else {
-        document.write(pokemonList[i].name + ' (height ' + pokemonList[i].height + ') Your Pokemon is small!');
-        document.write('<br>')
-    }
-     
-}
