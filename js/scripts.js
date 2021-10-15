b50f32cd175115dcb800227cb027638aa61fd8ba
moni-tu <turani.monica@gmail.com>
2021-10-15 21:01:16 +0200

    refactor: add IIFE and new variable
    add IIFE and create new variable pokemonRepository to hold what IIFE will return.
    Make IIFE return an object with two functions: getAll and add, according to instructions exercise 1.5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
-let pokemonList = [
+let pokemonRepository = (function() {
+    let pokemonList = [
     {name:'Bulbasaur', height:0.7, type: ['grass','poison']},
     {name:'Charmender', height:0.6, type: ['fire']},
     {name:'Diglett', height:0.2, type: ['earth']},
     {name:'Jigglipuff', height:0.5, type: ['fairy', 'normal']},
     {name:'Gastly', height:1.3, type: ['ghost','poison']}
-]
+    ];
+
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        }
+    };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Psyduck', height: 0.8});
+console.log(pokemonRepository.getAll());
 
 pokemonList.forEach(function(pokemon) {
     console.log(pokemon.name + ', height: ' + pokemon.height + ', type: ' + pokemon.type);
