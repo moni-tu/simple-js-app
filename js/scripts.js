de53b9c8343b89830f48b827fadb2bfe05bf2209
moni-tu <turani.monica@gmail.com>
2021-10-20 14:43:27 +0200

    re-factor: add <ul> and <button> to pokemonList
    As instructed by assignment 1.5, use Java to create an unordered list and list items with buttons that correspond to each pokemon.
    use document.createElement('li')
    document.createElement('button')
    button.classList.add
    Append button to listItem and append listItem to unorderedList

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,20 @@ pokemonRepository.add({name: 'Psyduck', height: 0.8});
 console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-    console.log(pokemon.name + ', height: ' + pokemon.height + ', type: ' + pokemon.type);
+    let unorderedList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText(pokemonList.name);
+    button.classList.add('button')
+    //the button is only virtually present. you have to append it to an element on the page
+    //so to make it present on the page. = CSSclass.appendChild(button)
+    listItem.appendChild(button);
+    unorderedList.append(listItem);
+
+
+
 });
+
+
+console.log(pokemon.name + ', height: ' + pokemon.height + ', type: ' + pokemon.type);
 
