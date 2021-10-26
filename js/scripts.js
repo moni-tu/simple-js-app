18bc43f0588e19a7c9c3d2ca90f77731f3b775f9
moni-tu <turani.monica@gmail.com>
2021-10-26 13:43:12 +0200

    re-factor: add fetch-polyfill and promise-polyfill to js folder
    add fetch-polyfill and promise-polyfill to js folder and link it to html page, according to assignment 1.7 API's Ajax and Asyncronous behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ let pokemonRepository = (function() {
     return {
         getAll: getAll,
         add: add,
-        addListItem: addListItem, 
+        addListItem: addListItem,   
     };
 
 
