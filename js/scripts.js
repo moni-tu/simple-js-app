92bf5704314abab60cd2ed29e571dd73366cf9d3
moni-tu <turani.monica@gmail.com>
2021-11-02 13:40:06 +0100

    re-vert to state of assignment 1.7
    Because of changes made trying to do assignment 1.8, I found bugs that I could not fix. So i revert to the last state I know were everything was working and start over with assignment 1.8.

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ let pokemonRepository = (function () {
       console.error(e);
     })
   }
-  
+
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
@@ -65,69 +65,6 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.add('is-visible');
-  
-    // Clear all existing modal content
-    modalContainer.innerHTML = '';
-  
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-  
-    // Add the new modal content
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-  
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-  
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-  
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-  
-    modalContainer.classList.add('is-visible');
-  
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-  }
-  
-  //show-modal is the id for the button
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-  
-  function hideModal(){
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();  
-    }
-  });
-
-  modalContainer.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
-
-
-
-  
   return {
     add: add,
     getAll: getAll,
@@ -136,9 +73,9 @@ let pokemonRepository = (function () {
     loadDetails: loadDetails,
     showDetails: showDetails
   };
-
 })();
-  
+
+
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
