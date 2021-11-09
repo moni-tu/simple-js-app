a72da59ae2ff95aa3d1792187d6c7045a96ef8e3
moni-tu <turani.monica@gmail.com>
2021-11-09 21:21:40 +0100

    style: set margins to zero
    After creating div for page container, the container had margins that did not look good in the app. Trying to fix this.

--- js/scripts.js
+++ js/scripts.js
@@ -67,7 +67,7 @@ let pokemonRepository = (function () {
     modalContainer.innerHTML = '';
     let modal = document.createElement('div');
     modal.classList.add('modal');
-
+    
     let closeButtonElement = document.createElement('button');
     closeButtonElement.classList.add('modal-close');
     closeButtonElement.classList.add("btn", "btn-outline-secondary");
