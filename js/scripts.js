b439b5328a4b46b3c9649dcd11afec4c7f4488ad
moni-tu <turani.monica@gmail.com>
2021-11-10 20:30:47 +0100

    undo: eliminate bootstrap class btn-block
    Btn block extends the button to the whole page (the parent element of pokemon list) so it does not look good

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ let pokemonRepository = (function () {
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
-    button.classList.add("btn", "btn-primary", "btn-lg", "btn-block");
+    button.classList.add("btn", "btn-primary", "btn-lg");
     listpokemon.classList.add("group-list-item");
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
