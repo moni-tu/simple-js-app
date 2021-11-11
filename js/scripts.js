e18f53c31b0ea4da490633727199f62d24b65d03
moni-tu <turani.monica@gmail.com>
2021-11-11 21:59:28 +0100

    fix: add image Url for both front and back of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -53,13 +53,15 @@ let pokemonRepository = (function () {
     })
   }
 
+  // Load list of pokemons with details
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
       // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
+      item.imageUrlFront = details.sprites.front_default;
+      item.imageUrlBack = details.sprites.back_default;
       item.height = details.height;
       item.types = details.types;
     }).catch(function (e) {
